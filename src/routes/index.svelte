4f94365b33a713845135a442c33fbcf5dcac05b0
shinhwan <you889@hanmail.net>
2022-11-19 17:27:36 +0900

    svelte JWT 인증 예제

---
+++ src/routes/index.svelte
@@ -0,0 +1,49 @@
+<script>
+    // import {onMount} from 'svelte';
+    import {authenticated} from '../stores/auth';
+
+    let message = ''
+
+    // onMount(async () => {
+    //     try {
+    //         const response = await fetch('http://localhost:8000/api/user', {
+    //             headers: {'Content-Type': 'application/json'},
+    //             credentials: 'include',
+    //         });
+
+    //         const content = await response.json();
+
+    //         message = `Hi ${content.name}`;
+    //         // $authenticated.set(true);
+	// 		$authenticated = true;
+    //     } catch (e) {
+			
+    //         message = 'You are not logged in';
+	// 		$authenticated = false;
+    //         // $authenticated.set(false);
+    //     }
+    // });
+
+	async function authChecker () {
+		if ($authenticated) {
+		const response = await fetch('http://localhost:8000/api/user', {
+                headers: {'Content-Type': 'application/json'},
+                credentials: 'include',
+            });
+
+            const content = await response.json();
+
+            message = `Hi ${content.name}`;
+            // $authenticated.set(true);
+			// $authenticated = false;
+		} else {
+			message = 'You are not logged in';
+			// $authenticated = true;
+		}
+	}
+
+	authChecker();
+
+</script>
+
+{message}
---
+++ src/routes/login.svelte
@@ -0,0 +1,41 @@
+
+<script>
+
+    import { goto } from "@sapper/app.mjs"
+    import {authenticated} from "../stores/auth";
+
+    let email = '', password = ''
+
+    const submit = async () => {
+        let res = await fetch('http://localhost:8000/api/login', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            credentials: 'include',
+            body: JSON.stringify({
+                email,
+                password
+            })
+        });
+        $authenticated = true;
+        console.log(res);
+        console.log(res);
+        console.log(res);
+        await goto('/')
+    }
+</script>
+
+<form on:submit|preventDefault={submit}>
+    
+    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+    
+        <input bind:value={email} type="email" class="form-control" placeholder="Email">
+    
+    
+        <input bind:value={password} type="password" class="form-control" placeholder="Password">
+        
+
+    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+</form>
+  
+  
